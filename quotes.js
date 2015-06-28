978fe01000aa547cd432d97bbdfafac0283b8f6b
simonf <simon@home.simonf.net>
2015-06-28 09:08:14 +0100

    Simplify promises code

--- quotes.js
+++ quotes.js
@@ -3,45 +3,39 @@ var fs = require('fs');
 var Q = require('q');
 
 module.exports = {
-	getRandomQuote: getRandomQuote
+  getRandomQuote: getRandomQuote
 };
 
 if (process.env.NODE_ENV === 'test') {
   module.exports._private = { 
-		loadQuotes: loadQuotes,
-		randomIntFromInterval: randomIntFromInterval
-	};
+    loadQuotes: loadQuotes,
+    randomIntFromInterval: randomIntFromInterval
+  };
 }
 
 var quotes = [];
 
 function loadQuotes() {
-	if(quotes.length > 0) {
-		return Q.fcall(function(){ return quotes;});
-	}
-	else {
-		var deferred = Q.defer();
-		fs.readFile('./data/quotes.json',{encoding:'utf-8'},function(err,data){
-			if(err) {
-				deferred.reject(err);
-			} else {
-				quotes = JSON.parse(data);
-				deferred.resolve(quotes);
-			}
-		});
-		return deferred.promise;
-	}
+  if(quotes.length > 0) {
+    return Q.fcall(function(){ return quotes;});
+  } else {
+    return Q.nfcall(fs.readFile,'./data/quotes.json','utf-8').
+    then(function(data){
+      quotes = JSON.parse(data);
+      return quotes; 
+    });
+  }
 }
 
 function getRandomQuote(){
-	return loadQuotes().then(function(qa){
-		var n = randomIntFromInterval(0,qa.length-1);
-		return qa[n];
-	});
+  return loadQuotes().then(function(qa){
+    var n = randomIntFromInterval(0,qa.length-1);
+    return qa[n];
+  });
 }
 
 function randomIntFromInterval(min,max) {
-    return Math.floor(Math.random()*(max-min+1)+min);
+  return Math.floor(Math.random()*(max-min+1)+min);
 }
 
 
